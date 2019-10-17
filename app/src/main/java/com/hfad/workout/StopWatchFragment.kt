d826aeee7c70abf076f31e035e00492fbd45ac33
klot3n <klot3n@gmail.com>
2019-10-17 20:48:21 +0500

    Fragments:   - как реализовать setOnClickListener?

--- app/src/main/java/com/hfad/workout/StopWatchFragment.kt
+++ app/src/main/java/com/hfad/workout/StopWatchFragment.kt
@@ -1,5 +1,6 @@
 package com.hfad.workout
 
+//import java.util.logging.Handler
 import android.os.Bundle
 import android.os.Handler
 import android.support.v4.app.Fragment
@@ -9,13 +10,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import kotlinx.android.synthetic.main.fragment_stopwatch.*
 import java.util.*
-//import java.util.logging.Handler
 
 class StopWatchFragment : Fragment() {
     var seconds = 0
     var running = false
     var wasRunning = false
-
+    var handler: Handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,36 +53,35 @@ class StopWatchFragment : Fragment() {
         outState.putBoolean("wasRunning", wasRunning)
     }
 
-    private fun onClickStart() {
-        running = true
-    }
-
-    private fun onClickStop() {
-        running = false
-    }
-
-    private fun onClickReset() {
-        running = false
-        seconds = 0
-    }
-    private fun runTimer (view :View){
+//    private fun onClickStart() {
+//        running = true
+//    }
+//
+//    private fun onClickStop() {
+//        running = false
+//    }
+//
+//    private fun onClickReset() {
+//        running = false
+//        seconds = 0
+//    }
+    private fun runTimer (view :View) {
         val timeView = view.findViewById<TextView>(R.id.time_view)
-
-        lateinit var handler:Handler
+//        start_button.setOnClickListener { running = true; start_button.text = "!!!!!!!!!!!" }
+//        stop_button.setOnClickListener { running = false }
+//        reset_button.setOnClickListener { running = false; seconds = 0 }
         handler.post( object : Runnable {
             override fun run() {
                 val hours = seconds/3600
                 val minutes = (seconds%3600)/60
                 val secs=seconds%60
-                val time=String.format(Locale.getDefault(),"%d:02d:%02d", hours,minutes,secs)
+                val time=String.format(Locale.getDefault(),"%d:%02d:%02d", hours,minutes,secs)
                 timeView.setText(time)
                 if(running){
                     seconds++
                 }
                 handler.postDelayed(this,1000)
-            }
+          }
         })
     }
-//    override fun onClick (v:View){
-
 }
